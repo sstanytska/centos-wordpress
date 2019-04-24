55c2cb8021ecc92572bd92a61680679523625d69
sstanytska <stanytskasofia@gmail.com>
2019-04-23 19:06:32 -0500

    edit some code

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,15 @@
 
-//
+
 properties([parameters([string(defaultValue: '5.0.4', description: 'Please provide version of the wordpress.', name: 'wordpressVersion', trim: true)]),
 
 pipeline{
     agent any
     stages {
-        //
+        
         stage('set version') {
           sh "export WORDPRESS_VERSION=${wordpressVersion}"
         }
-        //
+        
         stage("run packer"){
           steps{
                 sh "packer build image.json"
